2871e3e858abaa3cd54f4ce4d0ee6853124f2d26
thirteenpylons <thirteenpylons@gmail.com>
2021-10-15 21:56:57 -0400

    init

---
+++ __main__.py
@@ -0,0 +1,10 @@
+"""
+The script code for the application.
+
+Author: Christian M. Fulton
+Date:   15.Oct.2021
+"""
+
+
+import sys, app
+app.execute(sys.argv[1:])
---
+++ app.py
@@ -0,0 +1,12 @@
+"""
+Module that will launch browser to automate pickups.
+
+Author: Christian M. Fulton
+Date: 15.Oct.2021
+"""
+
+
+def execute():
+	"""
+	Executes the application
+	"""
