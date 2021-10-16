7bdcfe6f5a21ff02abf331edbdfea82fd760dd8a
thirteenpylons <thirteenpylons@gmail.com>
2021-10-15 23:30:42 -0400

    refactor

--- app.py
+++ app.py
@@ -23,25 +23,23 @@ def execute():
 	driver.get("https://www.etsy.com")
 	driver.implicitly_wait(15)
 	# locate login
-	sign_in = driver.find_element(By.XPATH value='/html/body/div[3]/header/div[4]/nav/ul/li[1]/button')
+	sign_in = driver.find_element(By.XPATH, value='/html/body/div[3]/header/div[4]/nav/ul/li[1]/button')
 	sign_in.click()
 	# locate input
-	email_in = driver.find_element(By.XPATH value='//*[@id="join_neu_email_field"]')
-	email_in.click()
+	email_in = driver.find_element(By.XPATH, value='//*[@id="join_neu_email_field"]')
 	email_in.send_keys(UNAME)
-	pwd_in = driver.find_element(By.XPATH value='//*[@id="join_neu_password_field"]')
-	pwd_in.click()
+	pwd_in = driver.find_element(By.XPATH, value='//*[@id="join_neu_password_field"]')
 	pwd_in.send_keys(PWD + '\ue007')
 
 	# locate shop button
-	shop_btn = driver.find_element(By.XPATH value='/html/body/div[3]/header/div[4]/nav/ul/li[2]/span/a/span[1]/svg')
+	shop_btn = driver.find_element(By.XPATH, value='/html/body/div[3]/header/div[4]/nav/ul/li[2]/span/a/span[1]/svg')
 	shop_btn.click()
 
 	# find sales tab
-	orders_btn = driver.find_element(By.XPATH value='/html/body/div[4]/div/div[1]/div/div[1]/div[3]/div/div[1]/div[2]/ul/li[5]/a/div/div[2]/span')
+	orders_btn = driver.find_element(By.XPATH, value='/html/body/div[4]/div/div[1]/div/div[1]/div[3]/div/div[1]/div[2]/ul/li[5]/a/div/div[2]/span')
 	orders_btn.click()
 
-	order_qty = driver.find_element(By.XPATH value='/html/body/div[4]/div/div[1]/div/div[3]/div/div[3]/div/div/div[1]/div[2]/nav/ul/li[1]/a/span[2]').text
+	order_qty = driver.find_element(By.XPATH, value='/html/body/div[4]/div/div[1]/div/div[3]/div/div[3]/div/div/div[1]/div[2]/nav/ul/li[1]/a/span[2]').text
 
 
 	driver.quit()
